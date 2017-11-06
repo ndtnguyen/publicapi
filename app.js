79e5dad95dfb7aae32a243bf1cdf9579bd410a1d
Nguyễn Đào Thảo Nguyên <ndtnguyen9596@gmail.com>
2017-11-06 12:33:32 +0700

    init

---
+++ app.js
@@ -0,0 +1,40 @@
+﻿'use strict';
+var debug = require('debug');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var handlebars = require('express-handlebars');
+var routes = require('./routes/index');
+var handlebars_sections = require('express-handlebars-sections');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', handlebars({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: 'views/',
+    helpers: {
+        section: handlebars_sections()
+    }
+}));
+
+app.set('view engine', 'hbs');
+// app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function () {
+    debug('Express server listening on port ' + server.address().port);
+});
